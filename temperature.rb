9973608f08f56e5b932d33330066d011dbcf36de
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-02 18:23:27 -0400

    Define method to convert fahrenheit to celcius

--- temperature.rb
+++ temperature.rb
@@ -6,10 +6,17 @@ class Temperature
 
   def to_fahrenheit
     if @temperature.has_key?(:c)
-      (@temperature[:c] * (9/5)) + 32
+      (@temperature[:c] * 9/5) + 32
     else
       @temperature[:f]
     end
   end
 
+  def to_celsius
+    if @temperature.has_key?(:f)
+      (@temperature[:f] - 32) * 5.0/9.0
+    else
+      @temperature[:c]
+    end
+  end
 end
